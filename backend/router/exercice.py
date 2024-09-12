d0dba70ad6ff0082b2163fec353ad1f72d9c8d06
Antoine-ValentinCharpentier <antoinevalentin.charpentier@gmail.com>
2024-09-12 22:28:43 +0200

    :sparkles: Added setup ollama

--- backend/router/exercice.py
+++ backend/router/exercice.py
@@ -1,8 +1,28 @@
 from fastapi import APIRouter, Request
 
+from langchain_ollama import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+from langchain.prompts.prompt import PromptTemplate
+
+import os
+
 router = APIRouter()
 
 @router.get("")
 async def get_exercise(request: Request):
     user_payload = getattr(request.state, "user", None)
-    return user_payload
+    
+    summary_template = """
+        Raconte une blague sur le thème de : {theme}
+    """
+    
+    summary_prompt_template = PromptTemplate(input_variables=["theme"], template=summary_template)
+    
+    llm = ChatOllama(model="llama3.1", temperature=1.0, base_url=os.getenv("OLLAMA_BASE_URL", "https://localhost:11434"))
+    
+    chain = summary_prompt_template | llm | StrOutputParser()
+    
+    theme = "Suédois"
+    res = chain.invoke(input={"theme": theme})
+    
+    return {"user_payload": user_payload, "joke":res}
